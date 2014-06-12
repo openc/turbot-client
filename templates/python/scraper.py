0882a9bf7a986e8e74bfbd37d2195185d7f0eaab
Seb Bacon <seb.bacon@gmail.com>
2014-06-12 13:33:01 +0100

    Fix generated bots to have valid schemas

--- templates/python/scraper.py
+++ templates/python/scraper.py
@@ -1,5 +1,9 @@
 import json
+import datetime
 
 for n in range(0,20):
-    data = {"number": n, "message": "Hello %s" % n}
+    data = {"number": n,
+            "message": "Hello %s" % n,
+            "sample_date": datetime.datetime.now().isoformat(),
+            "source_url": "http://somewhere.com/%s" % n}
     print json.dumps(data)
--- templates/ruby/scraper.rb
+++ templates/ruby/scraper.rb
@@ -1,6 +1,11 @@
 require 'json'
 
 (1...20).each do |n|
-  data = {number: n, message: "Hello #{n}"}
+  data = {
+    number: n,
+    message: "Hello #{n}",
+    sample_date: Time.now,
+    source_url: "http://somewhere.com/#{n}"
+  }
   puts JSON.dump(data)
 end
