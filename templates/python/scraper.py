4bcf46d3d4013fe3f95a893b5dd594c98d0e9a9a
Peter Inglesby <peter.inglesby@gmail.com>
2014-11-26 12:09:41 +0000

    Add company_fields to templates

--- templates/python/scraper.py
+++ templates/python/scraper.py
@@ -8,6 +8,7 @@ turbotlib.log("Starting run...") # Optional debug logging
 
 for n in range(0,20):
     data = {"number": n,
+            "company": "Company %s Ltd" % n,
             "message": "Hello %s" % n,
             "sample_date": datetime.datetime.now().isoformat(),
             "source_url": "http://somewhere.com/%s" % n}
