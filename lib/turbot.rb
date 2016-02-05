openc/turbot-client

commit d8e2d6b172c0f5277beb6e809262105f42f15e49

auth:whoami and auth:token error with "not logged in" if not logged in

lib/turbot/auth.rb
@@ -52,28 +52,40 @@ class Turbot::Auth
     end
 
     def reauthorize
-      @credentials = ask_for_and_save_credentials
+      self.credentials = ask_for_and_save_credentials
     end
 
-    def user    # :nodoc:
-      get_credentials[0]
+    def user(ask_for_credentials = true)    # :nodoc:
+      get_credentials(ask_for_credentials)[0]
     end
 
-    def password    # :nodoc:
-      get_credentials[1]
+    def password(ask_for_credentials = true)    # :nodoc:
+      get_credentials(ask_for_credentials)[1]
     end
 
-    def api_key
-      api.get_api_key
+    def api_key(ask_for_credentials = true)
+      api.get_api_key(ask_for_credentials)
     end
 
     def api_key_for_credentials(user = get_credentials[0], password = get_credentials[1])
-      api = Turbot::API.new(default_params)
-      api.get_api_key_for_credentials(user, password)["api_key"]
+      user, password = get_credentials(ask_for_credentials)
+      unless user.empty? && password.empty?
+        api = Turbot::API.new(default_params)
+        api.get_api_key_for_credentials(user, password)["api_key"]
+      end
     end
 
-    def get_credentials    # :nodoc:
-      @credentials ||= (read_credentials || ask_for_and_save_credentials)
+    def get_credentials(ask_for_credentials = true)    # :nodoc:
+      self.credentials ||= begin
+        value = read_credentials
+        if value
+          value
+        elsif ask_for_credentials
+          ask_for_and_save_credentials
+        else
+          ['', '']
+        end
+      end
     end
 
     def delete_credentials
@@ -82,7 +94,9 @@ class Turbot::Auth
         netrc.delete("code.#{host}")
         netrc.save
       end
-      @api, @client, @credentials = nil, nil
+      @api = nil
+      @client = nil
+      self.credentials = nil
     end
 
     def netrc_path
@@ -111,20 +125,16 @@ class Turbot::Auth
     def read_credentials
       if ENV['TURBOT_API_KEY']
         ['', ENV['TURBOT_API_KEY']]
-      else
-
+      elsif netrc
         # read netrc credentials if they exist
-        if netrc
-          # force migration of long api tokens (80 chars) to short ones (40)
-          # #write_credentials rewrites both api.* and code.*
-          credentials = netrc["api.#{host}"]
-          if credentials && credentials[1].length > 40
-            @credentials = [ credentials[0], credentials[1][0,40] ]
-            write_credentials
-          end
-
-          netrc["api.#{host}"]
+        # force migration of long api tokens (80 chars) to short ones (40)
+        # #write_credentials rewrites both api.* and code.*
+        value = netrc["api.#{host}"]
+        if value && value[1].length > 40
+          self.credentials = [ value[0], value[1][0,40] ]
+          write_credentials
         end
+        netrc["api.#{host}"]
       end
     end
 
@@ -134,8 +144,8 @@ class Turbot::Auth
       unless running_on_windows?
         FileUtils.chmod(0600, netrc_path)
       end
-      netrc["api.#{host}"] = self.credentials
-      netrc["code.#{host}"] = self.credentials
+      netrc["api.#{host}"] = credentials
+      netrc["code.#{host}"] = credentials
       netrc.save
     end
 
@@ -193,7 +203,7 @@ class Turbot::Auth
       begin
         # ask for username and password, look up API key against API given these
         # In looking up the API key it also attempts to log the user in
-        @credentials = ask_for_credentials
+        self.credentials = ask_for_credentials
         # write these to a hidden file
         write_credentials
         check
@@ -206,7 +216,7 @@ class Turbot::Auth
         delete_credentials
         raise e
       end
-      @credentials
+      credentials
     end
 
     def retry_login?

lib/turbot/command/auth.rb
@@ -64,7 +64,12 @@ class Turbot::Command::Auth < Turbot::Command::Base
   def token
     validate_arguments!
 
-    display Turbot::Auth.api_key
+    api_key = Turbot::Auth.api_key
+    if api_key
+      display api_key
+    else
+      error "not logged in"
+    end
   end
 
   # auth:whoami
@@ -79,7 +84,12 @@ class Turbot::Command::Auth < Turbot::Command::Base
   def whoami
     validate_arguments!
 
-    display Turbot::Auth.user
+    user = Turbot::Auth.user(false)
+    if user.empty?
+      error "not logged in"
+    else
+      display user
+    end
   end
 
 end

spec/spec_helper.rb
@@ -64,9 +64,6 @@ def execute(command_line)
     stub(base).bot.returns("example")
   end
 
-  stub(Turbot::Auth).get_credentials.returns(['email@example.com', 'apikey01'])
-  stub(Turbot::Auth).api_key.returns('apikey01')
-
   original_stdin, original_stderr, original_stdout = $stdin, $stderr, $stdout
 
   $stdin  = captured_stdin  = StringIO.new
@@ -129,19 +126,6 @@ end
 
 def fail_command(message)
   raise_error(Turbot::Command::CommandFailed, message)
-end
-
-def stub_core
-  @stubbed_core ||= begin
-    stubbed_core = nil
-    any_instance_of(Turbot::Client) do |core|
-      stubbed_core = stub(core)
-    end
-    stub(Turbot::Auth).user.returns("email@example.com")
-    stub(Turbot::Auth).password.returns("pass")
-    stub(Turbot::Client).auth.returns("apikey01")
-    stubbed_core
-  end
 end
 
 def stub_pg

spec/turbot/command/auth_spec.rb
@@ -16,6 +16,18 @@ describe Turbot::Command::Auth do
   describe "auth:token" do
 
     it "displays the user's api key" do
+      Turbot::Auth.stub(:api_key).and_return('apikey01')
+
+      stderr, stdout = execute("auth:token")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+apikey01
+STDOUT
+    end
+
+    it "displays the user's api key" do
+      Turbot::Auth.stub(:api_key).and_return(nil)
+
       stderr, stdout = execute("auth:token")
       stderr.should == ""
       stdout.should == <<-STDOUT
@@ -26,11 +38,23 @@ STDOUT
 
   describe "auth:whoami" do
     it "displays the user's email address" do
+      Turbot::Auth.stub(:read_credentials).and_return(['email@example.com', 'apikey01'])
+
       stderr, stdout = execute("auth:whoami")
       stderr.should == ""
       stdout.should == <<-STDOUT
 email@example.com
 STDOUT
+    end
+
+    it "displays a message if not logged in" do
+      Turbot::Auth.stub(:read_credentials).and_return(false)
+
+      stderr, stdout = execute("auth:whoami")
+      stdout.should == ""
+      stderr.should == <<-STDERR
+ !    not logged in
+STDERR
     end
 
   end

spec/turbot/command/bots_spec.rb
@@ -58,6 +58,9 @@ puts JSON.dump(#{hash})
 
     describe "#info" do
       it "should succeed" do
+        Turbot::Auth.stub(:read_credentials).and_return(['email@example.com', 'apikey01'])
+        Turbot::Auth.stub(:api_key).and_return('apikey01')
+
         stub_api_request(:get, "/api/bots/example?api_key=apikey01").to_return({
           :body => json_encode({
             "data" => {

spec/turbot/command_spec.rb
@@ -19,7 +19,6 @@ end
 describe Turbot::Command do
   before {
     Turbot::Command.load
-    stub_core # setup fake auth
   }
 
   describe "parsing errors" do

commit b0a1ca414c8c6183d9be4dab43c6ed90cc01278e

Fix error in auth:login introduced in d8e2d6b1

lib/turbot.rb
@@ -6,8 +6,6 @@ require 'optparse'
 require 'shellwords'
 
 require 'netrc'
-require 'turbot_api'
-require 'turbot_runner'
 require 'zip'
 
 require 'turbot/helpers'

lib/turbot/auth.rb
@@ -1,3 +1,5 @@
+require 'turbot_api'
+
 class Turbot::Auth
   class << self
     include Turbot::Helpers
@@ -36,27 +38,42 @@ class Turbot::Auth
       self.credentials = ask_for_and_save_credentials
     end
 
-    def user(ask_for_credentials = true)    # :nodoc:
+    # Returns the users's name.
+    #
+    # @param [Boolean] ask_for_credentials whether to login if not logged in
+    # @return [String] the user's name
+    def user(ask_for_credentials = true)
       get_credentials(ask_for_credentials)[0]
     end
 
-    def password(ask_for_credentials = true)    # :nodoc:
+    # Returns the user's password.
+    #
+    # @param [Boolean] ask_for_credentials whether to login if not logged in
+    # @return [String] the user's password
+    def password(ask_for_credentials = true)
       get_credentials(ask_for_credentials)[1]
     end
 
-    def api_key(ask_for_credentials = true)
-      api.get_api_key(ask_for_credentials)
+    # Returns the user's API key.
+    #
+    # @return [String] the user's API key
+    def api_key
+      api.get_api_key
     end
 
     def api_key_for_credentials(user = get_credentials[0], password = get_credentials[1])
-      user, password = get_credentials(ask_for_credentials)
-      unless user.empty? && password.empty?
-        api = Turbot::API.new(default_params)
-        api.get_api_key_for_credentials(user, password)["api_key"]
-      end
+      api = Turbot::API.new(default_params)
+      api.get_api_key_for_credentials(user, password)["api_key"]
     end
 
-    def get_credentials(ask_for_credentials = true)    # :nodoc:
+    # Gets the user's name and password.
+    #
+    # Tries to read the user's credentials from `.netrc`, then asks for
+    # and saves the user's credentials if that's an option.
+    #
+    # @param [Boolean] ask_for_credentials whether to login if not logged in
+    # @return [Array<String>] the user's name and password, or an empty array
+    def get_credentials(ask_for_credentials = true)
       self.credentials ||= begin
         value = read_credentials
         if value
@@ -111,7 +128,7 @@ class Turbot::Auth
         # #write_credentials rewrites both api.* and code.*
         value = netrc["api.#{host}"]
         if value && value[1].length > 40
-          self.credentials = [ value[0], value[1][0,40] ]
+          self.credentials = [value[0], value[1][0, 40]]
           write_credentials
         end
         netrc["api.#{host}"]
@@ -187,7 +204,7 @@ class Turbot::Auth
         # write these to a hidden file
         write_credentials
         check
-      rescue RestClient::Unauthorized, Turbot::API::Errors::NotFound, Turbot::API::Errors::Unauthorized => e
+      rescue RestClient::Unauthorized => e
         delete_credentials
         display "Authentication failed."
         retry if retry_login?

lib/turbot/command.rb
@@ -190,7 +190,7 @@ module Turbot
         require 'rest_client'
         raise(error)
       end
-    rescue Turbot::API::Errors::Unauthorized, RestClient::Unauthorized
+    rescue RestClient::Unauthorized
       puts "Authentication failure"
       if ENV['TURBOT_API_KEY']
         exit 1
@@ -198,10 +198,6 @@ module Turbot
         run "login"
         retry
       end
-    rescue Turbot::API::Errors::NotFound => e
-      error extract_error(e.response.body) {
-        e.response.body =~ /^([\w\s]+ not found).?$/ ? $1 : "Resource not found"
-      }
     rescue RestClient::ResourceNotFound => e
       error extract_error(e.http_body) {
         e.http_body =~ /^([\w\s]+ not found).?$/ ? $1 : "Resource not found"
@@ -209,10 +205,8 @@ module Turbot
     rescue RestClient::PaymentRequired => e
       # We've repurposed a 402 as a general error
       error extract_error(e.http_body)
-    rescue Turbot::API::Errors::Timeout, RestClient::RequestTimeout
+    rescue RestClient::RequestTimeout
       error "API request timed out. Please try again, or contact support@turbot.com if this issue persists."
-    rescue Turbot::API::Errors::ErrorWithResponse => e
-      error extract_error(e.response.body)
     rescue RestClient::RequestFailed => e
       error extract_error(e.http_body)
     rescue CommandFailed => e

lib/turbot/command/bots.rb
@@ -1,3 +1,5 @@
+require 'turbot_runner'
+
 # manage bots (generate skeleton, validate data, submit code)
 #
 class Turbot::Command::Bots < Turbot::Command::Base